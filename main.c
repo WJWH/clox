cb95f81760e6d763c9733e7f763a740c1706eaad
Wander Hillen <wjw.hillen@gmail.com>
2024-11-01 11:54:39 +0100

    Update example program in main

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 int main(int argc, const char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
+  int constant = addConstant(&chunk, 1.2); // make a new constant in the constant pool of the chunk
+  writeChunk(&chunk, OP_CONSTANT); // add OP_constant
+  writeChunk(&chunk, constant);    // next byte is index of the constant in the constant pool of the chunk
   writeChunk(&chunk, OP_RETURN);
 
   disassembleChunk(&chunk, "test chunk");
