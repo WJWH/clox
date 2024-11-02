ec9a3c121a5f45fddbe912eee012cc0aed1d5088
Wander Hillen <wjw.hillen@gmail.com>
2024-11-02 12:22:18 +0100

    Add the common arithmetic operators

--- main.c
+++ main.c
@@ -7,11 +7,22 @@ int main(int argc, const char* argv[]) {
   initVM();
   Chunk chunk;
   initChunk(&chunk);
-  int constant = addConstant(&chunk, 1.2); // make a new constant in the constant pool of the chunk
-  writeChunk(&chunk, OP_CONSTANT,123); // add OP_constant
-  writeChunk(&chunk, constant,123);    // next byte is index of the constant in the constant pool of the chunk
-  writeChunk(&chunk, OP_NEGATE, 123);
-  writeChunk(&chunk, OP_RETURN,123);
+  int constant = addConstant(&chunk, 1.2); // push 1.2
+  writeChunk(&chunk, OP_CONSTANT,123);
+  writeChunk(&chunk, constant,123);
+  constant = addConstant(&chunk, 3.4); // push 3.4
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123); // stack is now 1.2+3.4=4.6
+
+  constant = addConstant(&chunk, 5.6); // push 5.6
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123); // 4.6/5.6 = ~0.82
+  writeChunk(&chunk, OP_NEGATE, 123); // -0.82
+  writeChunk(&chunk, OP_RETURN,123);  // return
 
   disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
--- vm.c
+++ vm.c
@@ -30,6 +30,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
     // Section for debug tracing
@@ -52,6 +58,10 @@ static InterpretResult run() {
         break;
       }
       case OP_NEGATE: push(-pop()); break;
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
       case OP_RETURN: {
         printValue(pop());
         printf("\n");
@@ -60,6 +70,7 @@ static InterpretResult run() {
     }
   }
 
+#undef BINARY_OP
 #undef READ_BYTE
 #undef READ_CONSTANT
 }
