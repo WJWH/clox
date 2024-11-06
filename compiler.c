65ee6ae21a4a4bd1a57b53aba8d5f6d4e0fd5c16
Wander Hillen <wjw.hillen@gmail.com>
2024-11-06 13:25:31 +0100

    Error reporting functions for the compiler

--- compiler.c
+++ compiler.c
@@ -4,6 +4,47 @@
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct {
+  Token current;
+  Token previous;
+} Parser;
+
+Parser parser;
+
+static void errorAtCurrent(const char* message) {
+  errorAt(&parser.current, message);
+}
+
+static void error(const char* message) {
+  errorAt(&parser.previous, message);
+}
+
+static void errorAt(Token* token, const char* message) {
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_EOF) {
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // Nothing.
+  } else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void advance() {
+  parser.previous = parser.current;
+
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) break; // so usually (no error) the loop runs only once
+
+    errorAtCurrent(parser.current.start);
+  }
+}
+
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
   advance();
