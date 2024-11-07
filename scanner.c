694419532125cd09803afb4bd04d2f6c2656bfb3
Wander Hillen <wjw.hillen@gmail.com>
2024-11-07 13:09:19 +0100

    Double whoops also forgot identifiers/keywords in the scanner

--- scanner.c
+++ scanner.c
@@ -185,6 +185,7 @@ Token scanToken() {
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
   char c = advance();
+  if (isAlpha(c)) return identifier();
   if(isDigit(c)) return number();
   switch (c) {
     // single character tokens
