045d36bfa22c0a14a5855f5c1c3c35dec6060b16
Wander Hillen <wjw.hillen@gmail.com>
2024-11-09 13:19:18 +0100

    Add setting values in hash tables

--- table.c
+++ table.c
@@ -6,6 +6,8 @@
 #include "table.h"
 #include "value.h"
 
+#define TABLE_MAX_LOAD 0.75
+
 void initTable(Table* table) {
   table->count = 0;
   table->capacity = 0;
@@ -15,4 +17,70 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
   FREE_ARRAY(Entry, table->entries, table->capacity);
   initTable(table);
+}
+
+// linear probing until we find the key or an empty bucket
+// if we find an empty bucket before finding the key, the key is not yet present in the table
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+  uint32_t index = key->hash % capacity;
+  for (;;) {
+    Entry* entry = &entries[index];
+    if (entry->key == key || entry->key == NULL) {
+      return entry;
+    }
+
+    index = (index + 1) % capacity;
+  }
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+  // allocate a new array
+  Entry* entries = ALLOCATE(Entry, capacity);
+  // set all entries in the new array to the empty entry
+  for (int i = 0; i < capacity; i++) {
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL;
+  }
+
+  // for all non-empty entries, insert them into the new array
+  for (int i = 0; i < table->capacity; i++) {
+    Entry* entry = &table->entries[i];
+    if (entry->key == NULL) continue;
+
+    Entry* dest = findEntry(entries, capacity, entry->key);
+    dest->key = entry->key;
+    dest->value = entry->value;
+  }
+
+  // now we can free the memory for the old array
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  // and set the new values for entries and capacity
+  table->entries = entries;
+  table->capacity = capacity;
+}
+
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+    int capacity = GROW_CAPACITY(table->capacity);
+    adjustCapacity(table, capacity);
+  }
+
+  Entry* entry = findEntry(table->entries, table->capacity, key); // find a bucket for this key
+  bool isNewKey = entry->key == NULL; // key not yet present
+  if (isNewKey) table->count++; // updates don't increase the count but new insertions do
+
+  entry->key = key;
+  entry->value = value;
+  return isNewKey;
+}
+
+// add all the entries of the "from" table to the "to" table
+void tableAddAll(Table* from, Table* to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry* entry = &from->entries[i];
+    if (entry->key != NULL) {
+      tableSet(to, entry->key, entry->value);
+    }
+  }
 }
