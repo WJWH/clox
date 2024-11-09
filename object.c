ef9e560bd8718048ef7fa4c3671c3307f3033d7f
Wander Hillen <wjw.hillen@gmail.com>
2024-11-09 12:59:07 +0100

    Add hash code field to ObjString and handle it everywhere

--- object.c
+++ object.c
@@ -20,22 +20,35 @@ static Obj* allocateObject(size_t size, ObjType type) {
   return object;
 }
 
-static ObjString* allocateString(char* chars, int length) {
+static ObjString* allocateString(char* chars, int length, uint32_t hash) {
   ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
   string->length = length;
   string->chars = chars;
+  string->hash = hash;
   return string;
 }
 
+// the FNV-1a hash function:
+static uint32_t hashString(const char* key, int length) {
+  uint32_t hash = 2166136261u;
+  for (int i = 0; i < length; i++) {
+    hash ^= (uint8_t)key[i];
+    hash *= 16777619;
+  }
+  return hash;
+}
+
 ObjString* copyString(const char* chars, int length) {
+  uint32_t hash = hashString(chars, length);
   char* heapChars = ALLOCATE(char, length + 1); // allocate some new memory
   memcpy(heapChars, chars, length); // copy the chars to the new memory
   heapChars[length] = '\0'; // add null terminator
-  return allocateString(heapChars, length);
+  return allocateString(heapChars, length, hash);
 }
 
 ObjString* takeString(char* chars, int length) {
-  return allocateString(chars, length);
+  uint32_t hash = hashString(chars, length);
+  return allocateString(chars, length, hash);
 }
 
 void printObject(Value value) {
