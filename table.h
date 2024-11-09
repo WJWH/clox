bf94c6e10662217c565d7b3e2117281fa5a0ac07
Wander Hillen <wjw.hillen@gmail.com>
2024-11-09 12:59:33 +0100

    Fleshing hash table setup and freeing

--- table.h
+++ table.h
@@ -5,9 +5,17 @@
 #include "value.h"
 
 typedef struct {
+  ObjString* key;
+  Value value;
+} Entry;
+
+typedef struct {
   int count;
   int capacity;
   Entry* entries;
 } Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
 
 #endif
