60ea47b384f95c6d89fe2ca192cc8e2421f239af
Wander Hillen <wjw.hillen@gmail.com>
2024-11-10 13:32:26 +0100

    Get started adding print statements to the compiler

--- compiler.c
+++ compiler.c
@@ -41,6 +41,8 @@ typedef struct {
 } ParseRule;
 
 static void expression();
+static void statement();
+static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
@@ -94,6 +96,16 @@ static void consume(TokenType type, const char* message) {
   }
 
   errorAtCurrent(message);
+}
+
+static bool check(TokenType type) {
+  return parser.current.type == type;
+}
+
+static bool match(TokenType type) {
+  if (!check(type)) return false;
+  advance();
+  return true;
 }
 
 static void emitByte(uint8_t byte) {
@@ -261,6 +273,22 @@ static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+// statements of all types
+static void printStatement() {
+  expression();
+  consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+  emitByte(OP_PRINT);
+}
+
+static void declaration() {
+  statement();
+}
+
+static void statement() {
+  if(match(TOKEN_PRINT)) {
+    printStatement();
+  }
+}
 
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
@@ -270,8 +298,11 @@ bool compile(const char* source, Chunk* chunk) {
   parser.panicMode = false;
 
   advance();
-  expression();
-  consume(TOKEN_EOF, "Expect end of expression.");
+  // a program is a list of declarations, which may or may not actually just be a statement
+  while (!match(TOKEN_EOF)) {
+    declaration();
+  }
+
   endCompiler();
   return !parser.hadError;
 }
