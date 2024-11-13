f4d365d3b72c916f7524ae14d67a5f8452cb3b00
Wander Hillen <wjw.hillen@gmail.com>
2024-11-13 12:19:06 +0100

    Add JUMP_IF_FALSE opcode

--- vm.c
+++ vm.c
@@ -81,6 +81,7 @@ static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT())
+#define READ_SHORT() (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define BINARY_OP(valueType, op) \
     do { \
       if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
@@ -194,6 +195,11 @@ static InterpretResult run() {
         vm.stack[slot] = peek(0); // write top of stack to the local, keeping it at top of stack too (assignments are expessions!!)
         break;
       }
+      case OP_JUMP_IF_FALSE: {
+        uint16_t offset = READ_SHORT();
+        if (isFalsey(peek(0))) vm.ip += offset;
+        break;
+      }
       case OP_RETURN: {
         // Exit interpreter.
         return INTERPRET_OK;
@@ -204,6 +210,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_SHORT
 #undef READ_STRING
 }
 
