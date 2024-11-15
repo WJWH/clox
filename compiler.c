4e5987f9843a46e4f725807ee14fcb7fa0bcd24e
Wander Hillen <wjw.hillen@gmail.com>
2024-11-15 11:43:53 +0100

    Compiling of function declarations, no args yet

--- compiler.c
+++ compiler.c
@@ -347,6 +347,9 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+  // return early if we're in the top level scope. Never happens for variables, but does happen for functions
+  if (current->scopeDepth == 0) return;
+
   // set the depth of the latest local to the depth of the current scope (from -1, which it got from being declared in addLocal)
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
@@ -524,6 +527,27 @@ static void varDeclaration() {
   defineVariable(global);
 }
 
+static void function(FunctionType type) {
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope(); // doesn't need closing because the entire compiler will go out of scope
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+  // no parsing of args yet
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block(); // the function body is just a block. block() also consumes the closing }
+
+  ObjFunction* function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
+}
 
 static void printStatement() {
   expression();
@@ -642,7 +666,9 @@ static void ifStatement() {
 
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  } else if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
     statement();
