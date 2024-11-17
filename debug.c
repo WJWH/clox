8b129cc0235c8f163400bedbcf2f87db5ab77412
Wander Hillen <wjw.hillen@gmail.com>
2024-11-17 13:30:59 +0100

    Printing of upvalues for closures in disassembly

--- debug.c
+++ debug.c
@@ -103,8 +103,19 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       printf("%-16s %4d ", "OP_CLOSURE", constant);
       printValue(chunk->constants.values[constant]); // also print the constant
       printf("\n");
+
+      ObjFunction* function = AS_FUNCTION(chunk->constants.values[constant]);
+      for (int j = 0; j < function->upvalueCount; j++) {
+        int isLocal = chunk->code[offset++];
+        int index = chunk->code[offset++];
+        printf("%04d      |                     %s %d\n", offset - 2, isLocal ? "local" : "upvalue", index);
+      }
       return offset;
     }
+    case OP_GET_UPVALUE:
+      return byteInstruction("OP_GET_UPVALUE", chunk, offset);
+    case OP_SET_UPVALUE:
+      return byteInstruction("OP_SET_UPVALUE", chunk, offset);
     default:
       printf("Unknown opcode %d\n", instruction);
       return offset + 1;
