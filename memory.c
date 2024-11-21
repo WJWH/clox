3ad603fe9f5ac1e8ddf5662bb51084d3e239ecc9
Wander Hillen <wjw.hillen@gmail.com>
2024-11-21 12:46:13 +0100

    Freeing bound methods

--- memory.c
+++ memory.c
@@ -103,6 +103,12 @@ static void blackenObject(Obj* object) {
       markTable(&instance->fields);
       break;
     }
+    case OBJ_BOUND_METHOD: {
+      ObjBoundMethod* bound = (ObjBoundMethod*)object;
+      markValue(bound->receiver);
+      markObject((Obj*)bound->method);
+      break;
+    }
   }
 }
 
@@ -153,6 +159,9 @@ static void freeObject(Obj* object) {
       FREE(ObjInstance, object);
       break;
     }
+    case OBJ_BOUND_METHOD:
+      FREE(ObjBoundMethod, object);
+      break;
   }
 }
 
